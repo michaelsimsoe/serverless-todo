0b50ce9b14281ce30ac1b8da320069639d1ea97a
Michael Krøyserth-Simsø <mikke.korgen@gmail.com>
2021-04-10 08:04:40 +0200

    feat: implement get all todos

--- backend/src/dataLayer/TodoAccess.ts
+++ backend/src/dataLayer/TodoAccess.ts
@@ -1,6 +1,6 @@
-import * as AWS from 'aws-sdk'
-import { DocumentClient } from 'aws-sdk/clients/dynamodb'
-import { TodoItem } from '../models/TodoItem'
+import * as AWS from 'aws-sdk';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import { TodoItem } from '../models/TodoItem';
 
 export class TodoAccess {
   constructor(
@@ -8,17 +8,38 @@ export class TodoAccess {
     private readonly todoTable = process.env.TODO_TABLE
   ) {}
 
+  async getAllTodos(): Promise<TodoItem[]> {
+    console.log('Getting all todo');
+
+    const params = {
+      TableName: this.todoTable,
+      KeyConditionExpression: '#userId = :userId',
+      ExpressionAttributeNames: {
+        '#userId': 'userId'
+      },
+      ExpressionAttributeValues: {
+        ':userId': '1'
+      }
+    };
+
+    const result = await this.docClient.query(params).promise();
+    console.log(result);
+    const items = result.Items;
+
+    return items as TodoItem[];
+  }
+
   async createTodo(todoItem: TodoItem): Promise<TodoItem> {
-    console.log('Creating new todo')
+    console.log('Creating new todo');
 
     const params = {
       TableName: this.todoTable,
       Item: todoItem
-    }
+    };
 
-    const result = await this.docClient.put(params).promise()
-    console.log(result)
+    const result = await this.docClient.put(params).promise();
+    console.log(result);
 
-    return todoItem as TodoItem
+    return todoItem as TodoItem;
   }
 }
