7150233f12f3c0978e75ed3de9aa53dc12e5b280
Michael Krøyserth-Simsø <mikke.korgen@gmail.com>
2021-04-13 22:43:51 +0200

    feat: add middy to get todos

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -8,23 +8,31 @@ import {
 
 import { getAllTodos } from '../../businessLogic/todos';
 import { createLogger } from '../../utils/logger';
+import * as middy from 'middy';
+import { cors } from 'middy/middlewares';
 
 const logger = createLogger('createTodo');
 
-export const handler: APIGatewayProxyHandler = async (
-  event: APIGatewayProxyEvent
-): Promise<APIGatewayProxyResult> => {
-  logger.info('Processing Event ', event);
-  const authorization = event.headers.Authorization;
-  const split = authorization.split(' ');
-  const jwtToken = split[1];
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    logger.info('Processing Event ', event);
+    const authorization = event.headers.Authorization;
+    const split = authorization.split(' ');
+    const jwtToken = split[1];
 
-  const toDos = await getAllTodos(jwtToken);
+    const toDos = await getAllTodos(jwtToken);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      items: toDos
-    })
-  };
-};
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        items: toDos
+      })
+    };
+  }
+);
+
+handler.use(
+  cors({
+    credentials: true
+  })
+);
