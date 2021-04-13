4ddd63ea770a7726f7884fce336de0dcd5655450
Michael Krøyserth-Simsø <mikke.korgen@gmail.com>
2021-04-13 22:40:07 +0200

    feat: implement token based auth

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'fyqb5o0om3'
+export const apiEndpoint = `https://${apiId}.execute-api.eu-west-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'simsoe-serverless.eu.auth0.com', // Auth0 domain
+  clientId: 'U51B4Srimx8GfVCZDjPJnZnu5TNfnPLM', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
